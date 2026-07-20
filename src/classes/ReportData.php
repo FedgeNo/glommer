d3c92433cff41774dcc88abd7d05c41ce72b6ffd
FedgeNo <misterffoeg@hotmail.com>
2026-07-20 07:51:36 -0700

    ReportData: name the class that runs the join

--- src/classes/ReportData.php
+++ src/classes/ReportData.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * The columns Report's queries read off a Reports row (some fetch only a
  * subset of these - the rest just stay null). reporterUsername is the one
- * field that isn't a Reports column - it's rowsForAdmin()'s Users join.
+ * field that isn't a Reports column - it's ReportList's Users join.
  */
 class ReportData
 {
